a630646f21f6ecdba0d4d2d5eb82493d47bdfc83
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-03-09 14:39:47 +0700

    fix done

--- src/TH1/BubbleSort.java
+++ src/TH1/BubbleSort.java
@@ -1,27 +1,23 @@
 package TH1;
 
 public class BubbleSort {
-    static int[] list = {2, 3, 7, 4, 1, 9};
 
-    public static void bubbleSort(int[] list) {
-        boolean needNextPass = true;
-        for (int k = 1; k < list.length && needNextPass; k++) {
-            needNextPass = false;
-            for (int i = 0; i < list.length - k; i++) {
-                if (list[i] > list[i + 1]) {
-                    int temp = list[i];
-                    list[i] = list[i + 1];
-                    list[i + 1] = temp;
-                    needNextPass = true;
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = array.length - 1; j > i; j--)
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
                 }
-            }
         }
     }
 
     public static void main(String[] args) {
-        bubbleSort(list);
-        for (int i = 0; i < list.length; i++) {
-            System.out.print(list[i]+ " ");
+        int[] numbersArr = {5, 4, 1, 9, 8, 7, 6, 3, 2, 10};
+        bubbleSort(numbersArr);
+        for (int j : numbersArr) {
+            System.out.print(j + "\t");
         }
     }
 }
--- src/TH3/SelectionSort.java
+++ src/TH3/SelectionSort.java
@@ -16,6 +16,18 @@ public class SelectionSort {
         }
     }
 
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int value = array[i];
+            int j = i;
+            while (j > 0 && value < array[j - 1]) {
+                array[j] = array[j - 1];
+                j--;
+            }
+            array[j] = value;
+        }
+    }
+
     public static void main(String[] args) {
         int[] numbersArr = {5, 4, 1, 9, 8, 7, 6, 3, 2, 0};
         selectionSort(numbersArr);
