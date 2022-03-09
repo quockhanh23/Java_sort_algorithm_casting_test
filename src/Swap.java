4e3e956cfeeea839a5f99027a8e462093b7808e4
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-03-09 20:13:01 +0700

    done

---
+++ src/Swap.java
@@ -0,0 +1,25 @@
+public class Swap {
+
+    // hoán đổi giá trị không cần thêm biến
+    public static void swap1(int a, int b) {
+        System.out.println("a: " + a + ", b: " + b);
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        System.out.println("a: " + a + ", b: " + b);
+    }
+    // hoán đổi giá trị không cần thêm biến
+    public static void swap2(int a, int b) {
+        System.out.println("a: " + a + ", b: " + b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a: " + a + ", b: " + b);
+    }
+
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        Swap.swap1(a, b);
+        Swap.swap2(a, b);
+    }
+}
--- src/TH3/SelectionSort.java
+++ src/TH3/SelectionSort.java
@@ -3,11 +3,12 @@ package TH3;
 public class SelectionSort {
 
     public static void selectionSort(int[] array) {
-        for (int i = 0; i < array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             int minIndex = i;
-            for (int j = i + 1; j < array.length; j++)
+            for (int j = i + 1; j < array.length; j++) {
                 if (array[j] < array[minIndex])
                     minIndex = j;
+            }
             if (minIndex != i) {
                 int temp = array[minIndex];
                 array[minIndex] = array[i];
@@ -29,7 +30,7 @@ public class SelectionSort {
     }
 
     public static void main(String[] args) {
-        int[] numbersArr = {5, 4, 1, 9, 8, 7, 6, 3, 2, 0};
+        int[] numbersArr = {5, 6, 1, 9, 8, 7};
         selectionSort(numbersArr);
         for (int j : numbersArr) {
             System.out.print(j + "\t");
